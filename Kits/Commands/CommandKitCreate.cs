7fb9962367b60d37284b0b6361801f6b1fcc2231
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-10-07 02:37:01 +0700

    Add command /kit create

---
+++ Kits/Commands/CommandKitCreate.cs
@@ -0,0 +1,66 @@
+﻿using Kits.API;
+using OpenMod.API.Commands;
+using OpenMod.Core.Commands;
+using OpenMod.Extensions.Games.Abstractions.Players;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using IHasInventory = OpenMod.Extensions.Games.Abstractions.Entities.IHasInventory;
+
+namespace Kits.Commands
+{
+    [Command("create")]
+    [CommandActor(typeof(IPlayerUser))]
+    [CommandParent(typeof(CommandKit))]
+    [CommandSyntax("<name> [cooldown]")]
+    public class CommandKitCreate : Command
+    {
+        private readonly IKitManager m_KitManager;
+
+        public CommandKitCreate(IServiceProvider serviceProvider, IKitManager kitManager) : base(serviceProvider)
+        {
+            m_KitManager = kitManager;
+        }
+
+        protected override async Task OnExecuteAsync()
+        {
+            var playerUser = (IPlayerUser)Context.Actor;
+            if (Context.Parameters.Count >= 1)
+            {
+                throw new CommandWrongUsageException(Context);
+            }
+            var name = Context.Parameters[0];
+            float cooldown = 0;
+            if (Context.Parameters.Count > 1)
+            {
+                cooldown = await Context.Parameters.GetAsync<float>(1);
+            }
+
+            var kits = await m_KitManager.GetKits();
+            if (kits.Any(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase)))
+            {
+                // todo: add custom exception ArgumentException extended by UserFriendlyException
+                throw new UserFriendlyException("Kit with the same name already exists");
+            }
+
+            var hasInventory = (IHasInventory)playerUser;
+            if (hasInventory == null)
+            {
+                throw new NotSupportedException("IPlayer doesn't have compobillity IHasInventory");
+            }
+            var items = hasInventory.Inventory.SelectMany(x => x.Items.Select(c => c.Item));
+            if (!items.Any())
+            {
+                // todo: shows a message
+                return;
+            }
+            var kit = new Kit
+            {
+                Cooldown = cooldown,
+                Items = items.ToList(),
+                Name = name
+            };
+            await m_KitManager.AddKit(kit);
+        }
+    }
+}
