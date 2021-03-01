2d3e5bb6005e416049b3fe8a824927bc18d48dd1
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-03-02 01:41:55 +0700

    Add StringLocalizer into abstract of DatabaseCore, change plugin protected instead of private

--- Kits/Databases/KitDatabaseCore.cs
+++ Kits/Databases/KitDatabaseCore.cs
@@ -1,15 +1,19 @@
-﻿namespace Kits.Databases
-{
-    internal abstract class KitDatabaseCore
-    {
-        private readonly Kits m_Plugin;
+﻿using Autofac;
+using Microsoft.Extensions.Localization;
 
-        protected string TableName => m_Plugin.Configuration["database:connectionTableName"];
-        protected string Connection => m_Plugin.Configuration["database:connection"];
+namespace Kits.Databases
+{
+    public abstract class KitDatabaseCore
+    {
+        protected Kits Plugin { get; }
+        protected string TableName => Plugin.Configuration["database:connectionTableName"];
+        protected string Connection => Plugin.Configuration["database:connection"];
+        protected IStringLocalizer StringLocalizer { get; }
 
         protected KitDatabaseCore(Kits plugin)
         {
-            m_Plugin = plugin;
+            StringLocalizer = plugin.LifetimeScope.Resolve<IStringLocalizer>();
+            Plugin = plugin;
         }
     }
 }
