b914452696423e292054195fa1361d334cf139dc
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2021-04-10 12:34:48 +0700

    Remove LiteDB. Update OpenMod packages

--- Kits/API/Kit.cs
+++ Kits/API/Kit.cs
@@ -1,11 +1,10 @@
-﻿using LiteDB;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Kits.API
 {
     public class Kit
     {
-        [BsonId] public string? Name { get; set; }
+        public string? Name { get; set; }
         public float Cooldown { get; set; }
         public decimal Cost { get; set; }
         public decimal Money { get; set; }
